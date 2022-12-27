c97c8157d3a79adb9c5dfcafc2b3a5994ede391c
Tim Wilder <tmwilder@gmail.com>
2022-12-26 21:40:20 -0800

    Add correct routing/handling of basic event

--- internal/app/discord_interactions.go
+++ internal/app/discord_interactions.go
@@ -4,10 +4,44 @@ import (
 	"bytes"
 	"crypto/ed25519"
 	"encoding/hex"
+	"encoding/json"
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"io"
 	"net/http"
 )
+
+const COMMAND_QUEUED = "queue"
+const COMMAND_DEQUEUE = "dequeue"
+
+type DiscordUser struct {
+	Id       string `json:"id"`
+	Username string `json:"username"`
+}
+
+type DiscordMemberInfo struct {
+	User DiscordUser `json:"user"`
+}
+
+type OptionData struct {
+	Type  int    `json:"type"`
+	Name  string `json:"name"`
+	Value int    `json:"value"`
+}
+
+type InteractionData struct {
+	Options []OptionData `json:"options"`
+	Type    int          `json:"type"`
+	Name    string       `json:"name"`
+	Id      string       `json:"id"`
+}
+
+type Interaction struct {
+	Type   int               `json:"type"`
+	Token  string            `json:"token"`
+	Member DiscordMemberInfo `json:"member"`
+	Data   InteractionData   `json:"data"`
+}
 
 /*
 	Standard response to Discord Ping request.
@@ -25,16 +59,32 @@ func interactionsHandler(c *gin.Context) {
 	}
 	key := ed25519.PublicKey(decodedKey)
 
-	verified := verifyRequest(c, key)
+	requestBodyData, err := io.ReadAll(c.Request.Body)
+	if err != nil {
+		panic(err)
+	}
+	verified := verifyRequest(c, requestBodyData, key)
 	if !verified {
 		c.Status(http.StatusUnauthorized)
 		c.Writer.Write([]byte("invalid request signature"))
 	} else {
-		c.JSON(http.StatusOK, PingResponse{Type: 1})
+		interaction := parseInteraction(requestBodyData)
+		fmt.Println(interaction)
+		switch interaction.Type {
+		case 1:
+			c.JSON(http.StatusOK, PingResponse{Type: 1})
+			break
+		case 2:
+			message := handleInteractionCommand(interaction)
+			c.JSON(http.StatusOK, gin.H{"type": 4, "data": gin.H{"content": message}})
+			break
+		default:
+			fmt.Println(interaction)
+		}
 	}
 }
 
-func verifyRequest(c *gin.Context, key ed25519.PublicKey) bool {
+func verifyRequest(c *gin.Context, requestBodyData []byte, key ed25519.PublicKey) bool {
 	var toVerify bytes.Buffer
 
 	timestamp := c.Request.Header.Get("X-Signature-Timestamp")
@@ -46,11 +96,30 @@ func verifyRequest(c *gin.Context, key ed25519.PublicKey) bool {
 	if err != nil {
 		return false
 	}
-	jsonData, err := io.ReadAll(c.Request.Body)
 
+	toVerify.Write(requestBodyData)
+	return ed25519.Verify(key, toVerify.Bytes(), byteSig)
+}
+
+func parseInteraction(requestBodyData []byte) (interaction Interaction) {
+	err := json.Unmarshal(requestBodyData, &interaction)
 	if err != nil {
 		panic(err)
 	}
-	toVerify.Write(jsonData)
-	return ed25519.Verify(key, toVerify.Bytes(), byteSig)
+	return interaction
+}
+
+func handleInteractionCommand(interaction Interaction) (channelMessage string) {
+	switch interaction.Data.Name {
+	case COMMAND_QUEUED:
+		// TODO do matchmaking stuff here later
+		queueValue := interaction.Data.Options[0].Value
+		channelMessage = fmt.Sprintf("Queued with ratings range: %d", queueValue)
+		break
+	case COMMAND_DEQUEUE:
+		break
+	default:
+		panic("Unknown interaction: " + interaction.Data.Name)
+	}
+	return channelMessage
 }
