7cae0d377fbe98189737c94cc2a937f26e00aee6
Tim Wilder <tmwilder@gmail.com>
2022-12-27 17:54:44 -0800

    Add down migration

--- internal/db/migrations/1_initial_tables.down.sql
+++ internal/db/migrations/1_initial_tables.down.sql
@@ -1,1 +1,6 @@
+drop table if exists user_ratings_history;
+drop table if exists matches_history;
+drop table if exists matches;
+drop table if exists match_requests_history;
+drop table if exists match_requests;
 drop table if exists users;
